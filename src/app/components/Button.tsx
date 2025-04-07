3b2c0e73d4eaf14fe50296e09faf1dcd8153c2b0
Alexandr Petrovich <apalion97@gmail.com>
2025-04-07 16:05:50 +0300

    Add Button component with additional props and integrate it
    Introduced `Button` component enhancements, including optional `arrow` and `disabled` props for improved functionality. Updated the `Home` page to include examples of the `Button` with different configurations inside a new section layout.

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,15 +1,23 @@
 
 type ButtonProps = {
     title: string,
-    url: string,
     typeButton: "submit" | "reset" | "button" | undefined,
     ariaLabel: string,
+    arrow?: boolean,
+    disabled?: boolean,
 }
 
 const Button = (props:ButtonProps) => {
     return (
-        <button className="button" type={props.typeButton} aria-label={props.ariaLabel}>
+        <button className="button" type={props.typeButton} aria-label={props.ariaLabel} disabled={props.disabled}>
             <span>{props.title}</span>
+            {props.arrow && (
+                <svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"
+                     fill="none">
+                    <path d="M4.88452 0.880859L9.19988 5.00007L4.88452 9.11928" stroke="white" stroke-width="1.28571"
+                          stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            )}
         </button>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,27 @@ import Header from "@/app/components/Header";
 import BlogCard from "@/app/components/BlogCard";
 import MiniBlogCard from "@/app/components/MiniBlogCard";
 import LinkArrow from "@/app/components/LinkArrow";
+import Button from "@/app/components/Button";
 
 export default function Home() {
     return (
         <main className="">
             <Header/>
             <br/>
-            <BlogCard/>
-            <br/>
-            <MiniBlogCard/>
-            <LinkArrow title="Read more" url="#"/>
+            <section className="section">
+                <div className="container">
+                    <BlogCard/>
+                    <br/>
+                    <MiniBlogCard/>
+                    <br/>
+                    <LinkArrow title="Read more" url="#"/>
+                    <br/>
+                    <br/>
+                    <Button title="Button" typeButton={"button"} ariaLabel={"Button"}/>
+                    <br/>
+                    <Button title="Button" typeButton={"button"} ariaLabel={"Button"} disabled={true}/>
+                </div>
+            </section>
         </main>
     );
 }
